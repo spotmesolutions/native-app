197bd70d1ac761f1534e3c641b87f3a7e206cf9b
Minh Phan <minhphan156@gmail.com>
2018-04-21 15:03:10 -0700

    replace with working code

--- ios/SpotMe/AppDelegate.m
+++ ios/SpotMe/AppDelegate.m
@@ -1,25 +1,24 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
- * All rights reserved.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
  */
 
 #import "AppDelegate.h"
-#import GoogleMaps;
+
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
+@import GoogleMaps;
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyA_OYlfO3oJQKyqjSh0b2HEOIpdjBkEEhA"];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-[GMSServices provideAPIKey:@"AIzaSyCi03GTgQhep24XNpEJscDzINvWzwU2BgA"];
+
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"SpotMe"
                                                initialProperties:nil
