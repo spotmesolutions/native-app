909af94859d32b885a1736afb442573c863bdd21
Minh Phan <minhphan156@gmail.com>
2018-06-20 20:13:40 -0700

    test branch

--- src/components/Favorite.js
+++ src/components/Favorite.js
@@ -1,6 +1,6 @@
 import React,{Component} from "react";
 import { Text, View, Image, Button, Dimensions } from "react-native";
-
+//testttbranchh
 class Favorite extends Component{
 render(){
   return (
